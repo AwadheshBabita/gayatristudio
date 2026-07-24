209eb7d001d7dc3aa456ad316f3980130fafbd85
AwadheshBabita <gayatristudio840@gmail.com>
2026-07-24 15:29:22 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function askAI(){
+
+let question=document.getElementById("question").value;
+
+document.getElementById("answer").innerHTML=
+"<b>आपने पूछा:</b><br>"+question+
+"<br><br>अभी AI नहीं जोड़ा गया है।";
+
+}
